10bdaf3ba9ecceee97e4660fb3c053f8149ecb56
Mike Reining <emailmike@gmail.com>
2015-01-28 08:10:13 -0800

    Completed part 2 of ratings tutorial

--- PlayerDetailsTableViewController.swift
+++ PlayerDetailsTableViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class PlayerDetailsTableViewController: UITableViewController {
     var player: Player!
+    var game: String = "Chess"
 
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var detailLabel: UITableViewCell!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        detailLabel.text = game
+    }
+    
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         if indexPath.section == 0 {
@@ -24,6 +31,16 @@ class PlayerDetailsTableViewController: UITableViewController {
         if segue.identifier == "SavePlayerDetails" {
             player = Player(name: self.nameTextField.text, game: "Chess", rating: 1)
         }
+    }
+    
+    // Setup unwind segue when users save selected game
+    @IBAction func selectedGame(segue:UIStoryboardSegue) {
+        let gamePickerViewController = segue.sourceViewController as GamePickerTableViewController
+        if let selectedGame = gamePickerViewController.selectedGame {
+            detailLabel.text = selectedGame
+            game = selectedGame
+        }
+        self.navigationController?.popViewControllerAnimated(true)
     }
     
 }
