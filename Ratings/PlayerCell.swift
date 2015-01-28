02d2eff314da6903fdc86d47a02324eea5a8a380
Mike Reining <emailmike@gmail.com>
2015-01-27 22:50:17 -0800

    Part 1 of tutorial implemented successfully

---
+++ Ratings/PlayerCell.swift
@@ -0,0 +1,18 @@
+//
+//  PlayerCell.swift
+//  Ratings
+//
+//  Created by Michael Reining on 1/27/15.
+//  Copyright (c) 2015 Mike Reining. All rights reserved.
+//
+
+import UIKit
+
+class PlayerCell: UITableViewCell {
+    @IBOutlet weak var gameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    
+    
+    
+}
--- Ratings/PlayersTableViewController.swift
+++ Ratings/PlayersTableViewController.swift
@@ -20,12 +20,32 @@ class PlayersTableViewController: UITableViewController {
     }
  
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath) as UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("PlayerCell", forIndexPath: indexPath) as PlayerCell
         
         let player = players[indexPath.row] as Player
-        cell.textLabel?.text = player.name
-        cell.detailTextLabel?.text = player.game
+        
+        cell.nameLabel.text = player.name
+        cell.gameLabel.text = player.game
+        cell.ratingImageView.image = self.imageForRating(player.rating)
+        
         return cell
+    }
+    
+    func imageForRating(rating:Int) -> UIImage? {
+        switch rating {
+        case 1:
+            return UIImage(named: "1StarSmall")
+        case 2:
+            return UIImage(named: "2StarsSmall")
+        case 3:
+            return UIImage(named: "3StarsSmall")
+        case 4:
+            return UIImage(named: "4StarsSmall")
+        case 5:
+            return UIImage(named: "5StarsSmall")
+        default:
+            return nil
+        }
     }
     
     
